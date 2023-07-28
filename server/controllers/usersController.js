01d0aacad4db1bf93e820027d6bdaedcb649956a
hardik6869 <sorthiyahardik6869@gmail.com>
2023-07-28 15:40:23 +0530

    Register Page UI & Functionality

---
+++ server/controllers/usersController.js
@@ -0,0 +1,30 @@
+const User = require("../model/userModel");
+const bcrypt = require("bcrypt");
+
+module.exports.register = async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const usernameCheck = await User.findOne({ username });
+    if (usernameCheck) {
+      return res.json({ msg: "Username Already Exists", status: false });
+    }
+
+    const emailCheck = await User.findOne({ email });
+    if (emailCheck) {
+      return res.json({ msg: "Email Already Exists", status: false });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      email,
+      username,
+      password: hashedPassword,
+    });
+
+    delete user.password;
+    return res.json({ status: true, user });
+  } catch (ex) {
+    next(ex);
+  }
+};
