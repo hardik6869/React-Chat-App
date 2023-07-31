40d9837222326c6fa0775d689050818645888a25
hardik6869 <sorthiyahardik6869@gmail.com>
2023-07-31 19:21:38 +0530

    Set Avatar/Profile Picture

--- public/src/App.js
+++ public/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 import Chat from "./pages/Chat";
+import SetAvatar from "./pages/SetAvatar";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
+        <Route path="/setAvatar" element={<SetAvatar />} />
         <Route path="/chat" element={<Chat />} />
       </Routes>
     </BrowserRouter>
--- public/src/pages/Signin.jsx
+++ public/src/pages/Signin.jsx
@@ -25,7 +25,7 @@ const Signin = () => {
     if (localStorage.getItem("chat-app-user")) {
       navigate("/");
     }
-  }, []);
+  });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
