8f4808c8c69bd600a01d91857fd55afe6d92b353
hardik6869 <sorthiyahardik6869@gmail.com>
2023-08-07 14:23:05 +0530

    Integrate Socket.io

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/userRoutes");
 const messageRoutes = require("./routes/messagesRoute");
+const socket = require("socket.io");
 
 const app = express();
 require("dotenv").config();
@@ -27,4 +28,26 @@ mongoose
 
 const server = app.listen(process.env.PORT, () => {
   console.log(`Server Started on PORT ${process.env.PORT}`);
+});
+
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credential: true,
+  },
+});
+
+global.onlineUsers = new Map();
+
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+  socket.on("send-message", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("message-receive", data.message);
+    }
+  });
 });
